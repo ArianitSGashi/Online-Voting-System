49ea38559b39a6fbf3446037cba18c700158e968
endihyseni11 <endihyseni2509@gmail.com>
2024-07-31 14:55:41 +0200

    feat: i implemented Identity

--- OnlineVotingSystem/OnlineVotingSystem.Infrastructure/Startup.cs
+++ OnlineVotingSystem/OnlineVotingSystem.Infrastructure/Startup.cs
@@ -2,20 +2,31 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OnlineVotingSystem.Infrastructure.Persistence.Context;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace OnlineVotingSystem.Infrastructure
 {
     public static class Startup
     {
-        public static void ConfigureService (this IServiceCollection services)
+        public static void ConfigureService(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<ApplicationDBContext>(options =>
-            options.UseSqlServer("Your connection string"));
+
+
+           
+                services.AddDbContext<ApplicationDBContext>(options =>
+                options.UseSqlServer("Your connection string"));
+            
+
+
+
+            // Configure the AuthDbContext
+            services.AddDbContext<AuthDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Configure ASP.NET Core Identity
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
         }
     }
 }
