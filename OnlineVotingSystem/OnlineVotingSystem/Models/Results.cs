ff42df43eee2fb2985643fab3a52ef90147afe2b
bl3rt4 <blerta.aazemi@gmail.com>
2024-07-31 12:02:53 +0200

    Create  entity classes

---
+++ OnlineVotingSystem/OnlineVotingSystem/Models/Results.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static System.Collections.Specialized.BitVector32;
+
+namespace OnlineVotingSystem.Models
+{
+    public class Result
+    {
+        [Key]
+        public int ResultID { get; set; }
+
+        [ForeignKey("Election")]
+        public int ElectionID { get; set; }
+
+        [ForeignKey("Candidate")]
+        public int CandidateID { get; set; }
+
+        public int TotalVotes { get; set; }
+
+        // Navigation Properties
+        public Elections Elections { get; set; }
+        public Candidates Candidates { get; set; }
+    }
+}
