a17c3c449d30a4876fe9205759b8c6dfcbb3e2d7
amelaabd <amela.abdullahu28@gmail.com>
2024-08-01 15:40:50 +0300

    Create interfaces for entity

---
+++ OnlineVotingSystem/OnlineVotingSystem.Domain/Interfaces/IVotesRepository.cs
@@ -0,0 +1,21 @@
+﻿using OnlineVotingSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineVotingSystem.Domain.Interfaces
+{
+    
+    public interface IVotesRepository : IGenericRepository<Votes>
+    {
+        Task<IEnumerable<Votes>> GetByUserIdAsync(int userId);
+
+        Task<IEnumerable<Votes>> GetByElectionIdAsync(int electionId);
+
+        Task<IEnumerable<Votes>> GetByCandidateIdAsync(int candidateId);
+
+        Task<IEnumerable<Votes>> GetByVoteDateAsync(DateTime date);
+    }
+}
